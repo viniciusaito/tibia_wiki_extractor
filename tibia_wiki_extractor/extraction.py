0b6517e5b87fa0e0db179b257069416808955f57
vsaito <viniciusaitosilva@gmail.com>
2024-02-07 23:34:07 -0300

    Removing old function to get data from a row

--- tibia_wiki_extractor/extraction.py
+++ tibia_wiki_extractor/extraction.py
@@ -26,29 +26,6 @@ def apply_css_filters_on_selector(sel, filters):
 
     return sel
 
-def collect_text_from_selector_row(table_row_selector, filters, row_number=0, get_all=False):
-    """ Collect data from a selectors row, it can collect one or all elements inside that row.
-    Parameters
-    ----------
-    sel : sel
-        A parsel selector
-    sel : sel
-        A parsel selector
-    filters : list
-        List of filters that will be applied to the selector
-    """
-    if get_all:
-        text = (table_row_selector
-                    .css('td')[row_number]
-                    .css(f'{filters}::text')
-                    .getall())
-    else:
-        text = (table_row_selector
-                    .css('td')[row_number]
-                    .css(f'{filters}::text')
-                    .get())
-    return text
-
 def getall_text_from_table_row(table_row, row_number):
     text_data = apply_css_filters_on_selector(table_row, ['td'])
     table_data = text_data[row_number]
