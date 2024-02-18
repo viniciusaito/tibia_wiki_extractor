1133add962a143790db93553098cb4fce755f84d
vsaito <viniciusaitosilva@gmail.com>
2024-02-18 19:24:58 -0300

    Updating main, now it can extract all armors data

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tibia_wiki_extractor.extraction import *
+from tibia_wiki_extractor.database.connector import *
+from tibia_wiki_extractor.database.tables.armor import Armor
 
 url= 'https://www.tibiawiki.com.br/wiki/Armaduras'
 filters = ['table#tabelaDPL','tr']
@@ -7,26 +9,39 @@ table = apply_css_filters_on_selector(http_selector, filters)
 
 num_of_rows_in_table = len(table)
 
-# string_list = []
-# for row in range(1,num_of_rows_in_table):
-#     table_row = table[row]
-
-#     item_name = getall_text_from_table_row(table_row, 0)
-#     item_level = getall_text_from_table_row(table_row, 2)
-#     item_vocation = getall_text_from_table_row(table_row, 3)
-#     item_protection = getall_text_from_table_row(table_row, 7)
-    
-#     print(f'{row}: "{item_name}" "{item_level}" "{item_vocation}" "{item_protection}"')
-
-string_list_2 = []
-column_list = [0,2,3,4,5,6,7,8,9]
+armors = []
+column_list = {0:"name", 
+               2:"level", 
+               3:"vocation", 
+               4:"slots", 
+               5:"tier_class", 
+               6:"defense", 
+               7:"bonus", 
+               8:"protection", 
+               9:"weight"}
 for row in range(1,num_of_rows_in_table):
     string_list = []
     table_row = table[row]
     for column in column_list:
         data = getall_text_from_table_row(table_row, column)
         string_list.append(data)
-    string_list_2.append(string_list)
+    armors.append(string_list)
 
-for armor in string_list_2:
-    print(armor)
+engine = generate_database_engine()
+session = generate_database_session(engine)
+
+for armor in armors:
+    if armor[5] == '':
+        armor[5] = '0'
+    data_to_be_inserted = Armor(name = armor[0],
+                                level = int(armor[1]),
+                                vocation = armor[2],
+                                slots = int(armor[3]),
+                                tier_class = int(armor[4]),
+                                defense = int(armor[5]),
+                                bonus = armor[6],
+                                protection = armor[7],
+                                weight = float(armor[8]))
+    session.add(data_to_be_inserted)
+
+session.commit()
