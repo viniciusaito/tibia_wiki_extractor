5f6841e91f99e44119280a86e480dcb65dde5daa
vsaito <viniciusaitosilva@gmail.com>
2024-02-18 19:24:37 -0300

    creating table structure and changing database name to postgres

--- tibia_wiki_extractor/database/connector.py
+++ tibia_wiki_extractor/database/connector.py
@@ -1,18 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
+from sqlalchemy.orm import sessionmaker
 # Dev lib
 import os
 import dotenv
-
 dotenv.load_dotenv()
 
-url = URL.create(
-    drivername= "postgresql+psycopg",
-    username= os.getenv("db_user"),
-    host= os.getenv("db_host"),
-    database= "tibiawiki_extractor",
-    password= os.getenv("db_pwd"),
-    port= "5432"
-)
+def generate_database_engine():
+    url = URL.create(
+        drivername= "postgresql+psycopg",
+        username= os.getenv("db_user"),
+        host= os.getenv("db_host"),
+        database= "postgres",
+        password= os.getenv("db_pwd"),
+        port= "5432"
+    )
+    return create_engine(url, echo=True)
 
-engine = create_engine(url)
+def generate_database_session(engine):
+    Session = sessionmaker(bind=engine)
+    return Session()
